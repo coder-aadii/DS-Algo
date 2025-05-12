a80e51e8b3ab1d1ff21cc00f7fb9d3efa9b9e948
coder-aadii <aditya.aerpule191996@gmail.com>
2025-05-12 17:25:38 +0530

    added more practice

---
+++ Miscellaneous/FindLcm.java
@@ -0,0 +1,18 @@
+package Miscellaneous;
+
+public class FindLcm {
+    public static void main(String[] args) {
+        int a = 29;
+        int b = 4;
+        int lcm = (a > b) ? a : b;
+
+        while (true) {
+            if (lcm % a == 0 && lcm % b == 0) {
+                break;
+            }
+            lcm++;
+        }
+
+        System.out.printf("The Lowest common multiple of %d and %d is %d.\n", a, b, lcm);
+    }
+}
---
+++ Miscellaneous/SwapTwoNumbers.java
@@ -0,0 +1,20 @@
+package Miscellaneous;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 13, b = -25;
+        // int a = 44, b = 10;
+        // Goal => a = 25, b = 13
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        // a = a ^ b;
+        // b = a ^ b;
+        // a = a ^ b;
+
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+    }
+}
